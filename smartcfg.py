03260f11fe331e1583815d7a108b80ea510fd436
Dmitriy Doronin <d.y.doronin@tinkoff.ru>
2022-07-02 02:10:53 +0300

    Custom tag !IN_MODE added.

--- smartcfg.py
+++ smartcfg.py
@@ -1,13 +1,14 @@
 import os
+from pathlib import Path
 
 import yaml
 
 
+MODE_KEY = '_mode'
+MODES_KEY = '_modes'
+
+
 class ConfigError(Exception):
-    pass
-
-
-class ExtendedSafeLoader(yaml.SafeLoader):
     pass
 
 
@@ -15,19 +16,61 @@ def construct_env(loader, node):
     return os.environ[loader.construct_scalar(node)]
 
 
-def construct_text(loader, node):
-    path = loader.construct_scalar(node)
-    with open(path, 'r') as fr:
-        return fr.read().strip()
-
-
-ExtendedSafeLoader.add_constructor('!env', construct_env)
-ExtendedSafeLoader.add_constructor('!text', construct_text)
+def construct_dummy(loader, node):
+    return None
 
 
 class Config:
-    def __init__(self, stream):
+    def __init__(self, stream, base_dir):
+        def construct_text(loader, node):
+            path = loader.construct_scalar(node)
+            with open(Path(base_dir).parent / path, 'r') as fr:
+                return fr.read().strip()
+
+        class ExtendedSafeLoader(yaml.SafeLoader):
+            pass
+
+        ExtendedSafeLoader.add_constructor('!env', construct_env)
+        ExtendedSafeLoader.add_constructor('!text', construct_text)
+        ExtendedSafeLoader.add_constructor('!IN_MODE', construct_dummy)
+        cfg = yaml.load(stream, Loader=ExtendedSafeLoader)
+
+        construct_in_mode = self._get_in_mode_constructor(cfg)
+        ExtendedSafeLoader.yaml_constructors['!IN_MODE'] = construct_in_mode
         self._cfg = yaml.load(stream, Loader=ExtendedSafeLoader)
+
+    @staticmethod
+    def _get_in_mode_constructor(config):
+        not_given = object()
+        mode = config.get(MODE_KEY, not_given)
+        modes = config.get(MODES_KEY, not_given)
+        if mode is not_given and modes is not_given:
+            def construct_dont_use_in_mode(loader, node):
+                raise ConfigError(f'!IN_MODE tag is used but `{MODE_KEY}` '
+                                  f'and `{MODES_KEY}` keys are not provided')
+
+            return construct_dont_use_in_mode
+
+        if modes is not_given and mode is not not_given:
+            raise ConfigError(
+                f'`{MODE_KEY}` key is provided but `{MODES_KEY}` is not')
+        if mode is not_given and modes is not not_given:
+            raise ConfigError(
+                f'`{MODES_KEY}` key is provided but `{MODE_KEY}` is not')
+        if not isinstance(modes, list):
+            raise ConfigError(f'`{MODES_KEY}` key must be a list')
+        if mode not in modes:
+            raise ConfigError(
+                f'`{MODE_KEY}` value "{mode}" is not in `{MODES_KEY}`')
+
+        def construct_in_mode(loader, node):
+            mapping = loader.construct_mapping(node, deep=True)
+            for k in mapping:
+                if k not in modes:
+                    raise ConfigError(f'Value "{k}" is not in `{MODES_KEY}`')
+            return mapping[mode]
+
+        return construct_in_mode
 
     def __call__(self, path):
         path = path.split(sep='.')
--- tests/test_smartcfg.py
+++ tests/test_smartcfg.py
@@ -1,9 +1,12 @@
 from pathlib import Path
+
 import pytest
+
 from smartcfg import Config, ConfigError
 
 
 def test_load_dict():
+    base_dir = str(Path(__file__).resolve())
     cfg = Config("""
     a: 1
     b: hello
@@ -14,7 +17,7 @@ def test_load_dict():
       - another_key:
           key1: "value1"
           key2: 'value2'
-    """)
+    """, base_dir)
     assert cfg('a') == 1
     assert cfg('b') == 'hello'
     assert cfg('c.0') == 'one'
@@ -35,6 +38,7 @@ def test_load_dict():
 
 
 def test_load_env(set_env):
+    base_dir = str(Path(__file__).resolve())
     set_env('MY_VAR', 'hello')
     cfg = Config("""
     a: 1
@@ -46,11 +50,12 @@ def test_load_env(set_env):
       - another_key:
           key1: !env MY_VAR
           key2: 'value2'
-    """)
+    """, base_dir)
     assert cfg('c.3.another_key.key1') == 'hello'
 
 
 def test_load_text():
+    base_dir = str(Path(__file__).resolve())
     abs_path = Path(__file__).parent / 'files_to_load/some.txt'
     cfg = Config(f"""
         a: 1
@@ -62,12 +67,12 @@ def test_load_text():
           - another_key:
               key1: !text {abs_path}
               key2: 'value2'
-        """)
+        """, base_dir)
     assert cfg('c.3.another_key.key1') == (
         'We all came out to Mountreux\non the lake Geneva shoreline.'
     )
 
-    rel_path = abs_path.relative_to(Path.cwd())
+    rel_path = 'files_to_load/some.txt'
     cfg = Config(f"""
         a: 1
         b: hello
@@ -78,7 +83,93 @@ def test_load_text():
           - another_key:
               key1: !text {rel_path}
               key2: 'value2'
-        """)
+        """, base_dir)
     assert cfg('c.3.another_key.key1') == (
         'We all came out to Mountreux\non the lake Geneva shoreline.'
     )
+
+
+def test_in_mode_broken_config():
+    base_dir = str(Path(__file__).resolve())
+    with pytest.raises(ConfigError) as err:
+        Config("_mode: testing", base_dir)
+    assert str(err.value) == '`_mode` key is provided but `_modes` is not'
+    with pytest.raises(ConfigError) as err:
+        Config(
+            """
+                _modes:
+                  - testing
+                  - stable
+            """, base_dir)
+    assert str(err.value) == '`_modes` key is provided but `_mode` is not'
+    with pytest.raises(ConfigError) as err:
+        Config(
+            """
+                _mode: testing
+                _modes: testing
+            """, base_dir)
+    assert str(err.value) == '`_modes` key must be a list'
+    with pytest.raises(ConfigError) as err:
+        Config(
+            """
+                _mode: production
+                _modes:
+                  - testing
+                  - stable
+            """, base_dir)
+    assert str(err.value) == '`_mode` value "production" is not in `_modes`'
+    with pytest.raises(ConfigError) as err:
+        Config(
+            """
+                a: !IN_MODE
+                   testing: val1
+                   stable: val2
+            """, base_dir)
+    assert str(err.value) == ('!IN_MODE tag is used but `_mode` and '
+                              '`_modes` keys are not provided')
+
+
+def test_in_mode(set_env):
+    stream = ("""
+        _mode: !env MODE
+        _modes:
+          - testing
+          - stable
+        a: 1
+        b: hello
+        c:
+          - one
+          - two
+          - another_key: !IN_MODE
+                         testing: !text files_to_load/some.txt
+                         stable: 'value2'
+          - 3
+    """)
+    base_dir = str(Path(__file__).resolve())
+
+    set_env('MODE', 'testing')
+    cfg = Config(stream, base_dir)
+    assert cfg('_mode') == 'testing'
+    assert cfg('c.2.another_key') == (
+        'We all came out to Mountreux\non the lake Geneva shoreline.'
+    )
+
+    set_env('MODE', 'stable')
+    cfg = Config(stream, base_dir)
+    assert cfg('_mode') == 'stable'
+    assert cfg('c.2.another_key') == 'value2'
+
+
+def test_in_mode_unknown_mode():
+    base_dir = str(Path(__file__).resolve())
+    with pytest.raises(ConfigError) as err:
+        Config("""
+            _mode: testing
+            _modes:
+              - testing
+              - stable
+            another_key: !IN_MODE
+                         testing: !text files_to_load/some.txt
+                         production: 'value2'
+        """, base_dir)
+    assert str(err.value) == 'Value "production" is not in `_modes`'
